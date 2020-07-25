93b72874b4cc48cd41ac24c7ef4397e6bdea53d8
figa-Oz <agif@agifhost.localdomain>
2020-07-25 16:46:38 +0700

    Pass User Data and Loading Status with Promise.

---
+++ src/config/redux/action/index.js
@@ -0,0 +1,62 @@
+import firebase from '../../firebase'
+
+// set syncronous dispatch to asyncronous
+export const actionUser = () => (dispatch) => {
+    setTimeout(() => {
+      return dispatch({type: "CHANGE_USER", value: "admin"})
+    }, 2000)
+}
+
+export const registerUserApi = (data) => (dispatch) => {
+	
+	dispatch({type: "CHANGE_ISLOADING", value: true})
+
+	//Promise to give result (resolve:success or reject:failed)
+	return new Promise((resolve, reject) => {
+		firebase.auth().createUserWithEmailAndPassword(data.email, data.password)
+	  	.then(res => {
+	  		console.log("reponse: ", res)
+	  		dispatch({type: "CHANGE_ISLOADING", value: false})
+	  	})
+	  	.catch(function(error) {
+	  		var errorCode = error.code;
+	  		var errorMessage = error.message;
+	  		console.log(errorCode, errorMessage)
+
+	  		dispatch({type: "CHANGE_ISLOADING", value: false})
+		})
+	})
+
+}
+
+export const loginUserApi = (data) => (dispatch) => {
+
+	//Promise to give result (resolve:success or reject:failed)
+	return new Promise((resolve, reject) => {
+		dispatch({type: "CHANGE_ISLOADING", value: true})
+
+		firebase.auth().signInWithEmailAndPassword(data.email, data.password)
+	  	.then(res => {
+	  		console.log("reponse: ", res)
+	  		
+	  		const userData = {
+	  			uid 			: res.user.uid,
+	  			email 			: res.user.email,
+	  			emailVerified	: res.user.emailVerified
+	  		}
+
+	  		dispatch({type: "CHANGE_ISLOADING", value: true}) // display loading button
+	  		dispatch({type: "CHANGE_USER", value: userData}) // get user login data
+	  		resolve(true) // pass success to Login Content
+	  	})
+	  	.catch(function(error) {
+	  		var errorCode = error.code;
+	  		var errorMessage = error.message;
+	  		console.log(errorCode, errorMessage)
+
+	  		dispatch({type: "CHANGE_ISLOADING", value: false})
+	  		reject(false) // pass failed (invalid) to Login Content
+		})
+	})
+
+}
