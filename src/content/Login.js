89a0e0749740a0e641475a580791e3f098698c3f
figa-Oz <agif@agifhost.localdomain>
2020-07-25 16:51:23 +0700

    Set Await Promise to get response, status button and user data from redux

--- src/content/Login.js
+++ src/content/Login.js
@@ -1,50 +1,65 @@
 import React from 'react'
-import firebase from '../config/firebase'
+import Button from '../component/Button'
+import { loginUserApi } from '../config/redux/action'
+import { connect } from 'react-redux'
+
+// import {
+//   BrowserRouter as Router,
+//   Route
+// } from "react-router-dom"
+
+const reduxState = (state) => ({
+  isLoading: state.isLoading
+})
+
+const reduxDispatch = (dispatch) => ({
+  loginApi: (data) => dispatch(loginUserApi(data))
+})
 
 class Login extends React.Component {
-constructor(){
-	super()
-	this.state = {
-    	messages: [],
-    	email:'',
-    	password:'' 
+constructor(props){
+  super(props)
+  this.state = {
+      email:'',
+      password:'',
     }; 
 }
 
 handleChange = (e) => {
-  	console.log("e: ",e);
-  	this.setState({
-  		[e.target.id]: e.target.value
-  	});
+    this.setState({
+      [e.target.id]: e.target.value
+    });
   }
 
-handlelogin = (event) => {
-  	event.preventDefault();
-  	
-  	const { email, password } = this.state;
-  	console.log("data asli", email, password);
-  	
-  	firebase.auth().signInWithEmailAndPassword(email, password)
-  	.then(res => {
-  		console.log("reponse: ", res)
-  	})
-  	.catch(function(error) {
-  		// Handle Errors here.
-  		var errorCode = error.code;
-  		var errorMessage = error.message;
-  		// ...
-	});
+// Async - Await (Sinc to async proccess from dispatch & Wait proccess from Promise)
+handleLogin = async (event) => {
+    event.preventDefault();
+    
+    const { email, password } = this.state;
+    const { history } = this.props
+
+    // Await (Wait proccess from Promise)
+    const res = await this.props.loginApi({email, password}).catch(err => err)
+
+    if (res) { 
+      console.log("response login success", res)
+      this.setState({email:'', password:''})
+      history.push("/")
+    } else {
+      console.log("response login failed")
+    }
+
   }
 
   render() {
-  	return(
-  		<form onSubmit={this.handleLogin}>
-      		<input type="text" name="email" id="email" placeholder="email" onChange={this.handleChange} />
-      		<input type="password" name="password" id="password" placholder="password" onChange={this.handleChange} />
-      		<button type="submit">register</button>
-      	</form>
-  	)
+    return(
+      <div>
+          <input type="text" name="email" id="email" placeholder="email" onChange={this.handleChange} value={this.state.email} />
+          <input type="password" name="password" id="password" placholder="password" onChange={this.handleChange} value={this.state.password} />
+          <Button onClick={this.handleLogin} title="Login" loading={this.props.isLoading} />
+        </div>
+    )
   }
 }
 
-export default Login
+export default connect(reduxState, reduxDispatch)(Login)
